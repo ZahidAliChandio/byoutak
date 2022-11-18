9f6f6c3ef47846adaf0a04f781727d1b313718bd
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-19 03:18:59 +0500

    Contact form near to complete.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,9 @@ import Logo from "../../static/images/logo.png"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between py-4 md:py-6 px-12">
+    <div className="flex items-center justify-between py-3 md:py-4 px-5 sm:px-12">
       <span>
-        <img src={Logo} alt="logo" />
+        <img src={Logo} alt="logo" className="h-8 w-24 lg:w-auto lg:h-auto"/>
       </span>
       <span><i className="fas fa-bars  text-[red] text-lg md:text-xl lg:text-2xl"></i></span>
     </div>
--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ContactUsSvg from "../../static/images/contactUsIllustration.svg";
+import {ReactComponent as ContactUsSvg} from "../../static/images/contactUsIllustration.svg";
 import LocationPointer from "../../static/icons/contactlocation.png";
 import Clock from "../../static/icons/clock.png";
 import Phone from "../../static/icons/phone.png";
@@ -18,31 +18,31 @@ const ContactUs = () => {
           </h2>
         </div>
       </div>
-      <div className="flex flex-wrap gap-12 w-4/5 mx-auto mt-12">
-        <Card className="!block !w-full !bg-white !py-10 !px-8">
+      <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 w-11/12 md:w-[85%] mx-auto mt-12">
+        <Card className="!block !w-full md:!w-4/5 lg:!w-full !bg-white !py-10 px-7 lg:!px-12 !h-fit !m-auto">
           <div className="flex flex-col gap-3">
-            <h2 className="text-2xl text-[#FF5E00] font-bold">
+            <h2 className="text-xl text-[#FF5E00] font-bold">
               We are always eager to hear from you
             </h2>
-            <h3 className="text-xl text-[#767676]">
+            <h3 className="text-lg text-[#767676]">
               Need assistance ? Just write us a message
             </h3>
           </div>
           <form action="" className="w-full mt-12">
-            <div className="grid grid-cols-2 gap-4 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
               <div className="flex flex-col gap-8">
                 <input
                   type="text"
                   id="first_name"
-                  className="relative border-[#015C9A] bg-gray-50 border text-gray-900 text-sm rounded-lg  block w-full p-2.5 before:content['Name'] before:absolute before:-top-2 before:left-2 before:block before:text-white before:border-red-800 before:border-4"
+                  className="relative border-[#015C9A] bg-gray-50 border text-base lg:text-lg rounded-lg  block w-full p-2.5 before:content['Name'] before:absolute before:-top-2 before:left-2 before:block before:text-white before:border-red-800 before:border-4 outline-none"
                   placeholder="Name"
                   required
                 />
                 <PhoneInput
-                  placeholder="Enter phone number"
+                  placeholder="3012345678"
                   value={phone}
                   onChange={setPhone}
-                  defaultCountry="NL"
+                  defaultCountry="PK"
                 />
                 <select
                   className="block w-full bg-gray-50 border border-[#015C9A] text-gray-700 py-3 px-4 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -50,9 +50,11 @@ const ContactUs = () => {
                   id="time"
                   defaultValue="9:00 AM"
                 >
-                  <option value="11:00">Enter Location</option>
-                  <option value="12:00">12:00 AM</option>
-                  <option value="1:00">1:00 PM</option>
+                  <option value="Lahore" className="text-[gray]">Enter Location</option>
+                  <option value="Rawalpindi">Rawalpindi</option>
+                  <option value="Islamabad">Islamabad</option>
+                  <option value="Karachi">Karachi</option>
+                  <option value="Hyderabad">Hyderabad</option>
                 </select>
               </div>
               <div>
@@ -74,47 +76,46 @@ const ContactUs = () => {
             </div>
           </form>
         </Card>
-        <Card className="!block !w-full !bg-white !py-10 !px-8 -translate-y-28">
-          <div className="flex justify-center">
-            <img src={ContactUsSvg} alt="contactUsIllustration" />
+        <Card className="!block !bg-white !p-5 lg:-translate-y-28 !w-full md:!w-3/5 lg:!w-3/4 !m-auto">
+          <div className="flex justify-center">            
+            <ContactUsSvg transform="scale(1.2)"/>
           </div>
-          <h1 className="relative text-[#15314E] text-2xl font-bold -top-6 left-10">
+          <h1 className="relative text-[#15314E] text-[1.4rem] font-bold -top-2 left-10">
             How to reach us
           </h1>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 mt-1">
             <div className="flex items-center text-left gap-4">
-              <img src={LocationPointer} alt="location" />
+              <img src={LocationPointer} alt="location" className="h-10"/>
 
               <div>
-                <h2 className="text-base md:text-xl font-bold text-[#15314E]">
+                <h2 className="text-lg font-bold text-[#15314E]">
                   Address
                 </h2>
-                <p className="text-base lg:text-lg xl:text-xl text-[#767676]">
+                <p className="text-base xl:text-lg text-[#767676]">
                   47, North 90 Street, New Cairo, Egypt. <br />
                   Tower 2, Al Guezira Plaza, 6 of October, Egypt.
                 </p>
               </div>
             </div>
-            <div className="flex items-center text-left gap-6">
-              {/* <i class="fas fa-phone text-4xl text-[gray]"></i> */}
-              <img src={Phone} alt="phone" />
+            <div className="flex items-center text-left gap-6">              
+              <img src={Phone} alt="phone" className="w-7 h-7"/>
               <div>
-                <h2 className="text-base md:text-xl font-bold text-[#15314E]">
+                <h2 className="text-lg font-bold text-[#15314E]">
                   Contacts
                 </h2>
-                <p className="text-base lg:text-lg xl:text-xl text-[#767676]">
+                <p className="text-base xl:text-lg text-[#767676]">
                   +201065888849
                   <br /> info@nawy.com
                 </p>
               </div>
             </div>
             <div className="flex items-center text-left gap-4">
-              <img src={Clock} alt="phone" />
+              <img src={Clock} alt="phone" className="w-7 h-7"/>
               <div>
-                <h2 className="text-base md:text-xl font-bold text-[#15314E]">
+                <h2 className="text-lg font-bold text-[#15314E]">
                   Working Hours
                 </h2>
-                <p className="text-base lg:text-lg xl:text-xl text-[#767676]">
+                <p className="text-base xl:text-lg text-[#767676]">
                   Sunday-Thursday
                   <br /> 10:00 AM - 5:00 PM
                 </p>
