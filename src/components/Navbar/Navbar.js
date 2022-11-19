8cdec24cd32d48fc4155f4411684cd05fc0a6136
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-19 18:25:29 +0500

    navbar updated.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,23 +5,19 @@ import { Transition } from "react-transition-group";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
-  const nodeRef = useRef(null);
-  const navCheck = useRef(null);
 
   const onClickHandler = () => {
-    navCheck.current.checked ? setShowNav(true) : setShowNav(false);
+    setShowNav((prev) => (prev ? false : true));
   };
-  useEffect(() => {
-    window.innerWidth > 768 ? setShowNav(true) : setShowNav(false);
-  }, []);
-
+useEffect(()=>{
+  window.innerWidth>768?setShowNav(true):setShowNav(false);
+},[])
   return (
     <div className="flex items-center justify-between py-3 md:py-4 px-5 sm:px-12">
       <span>
         <img src={Logo} alt="logo" className="h-8 w-24 lg:w-auto lg:h-auto" />
       </span>
       <Transition
-        nodeRef={nodeRef}
         in={showNav}
         timeout={{ enter: 500, exit: 200 }}
         mountOnEnter
@@ -35,29 +31,22 @@ const Navbar = () => {
                 : state === "exiting"
                 ? "exit-animation"
                 : null
-            } flex gap-4 sm:gap-20 text-white text-lg font-semibold`}
+            } gap-4 sm:gap-20 text-white text-lg font-semibold md:!flex`}
           >
-            <li>
+            <li className="inline">
               <Link to="/">Home</Link>
             </li>
-            <li>Search</li>
-            <li>
+            <li className="inline mx-4 md:m-0">Search</li>
+            <li className="inline">
               <Link to="contactus">Contact us</Link>
             </li>
           </ul>
         )}
       </Transition>
-      <div className="block md:hidden" onClick={() => onClickHandler()}>
-        <label htmlFor="navCheck">
-          <i className="fas fa-bars  text-[red] text-lg md:text-xl lg:text-2xl"></i>
-        </label>
-        <input
-          type="checkbox"
-          id="navCheck"
-          className="hidden"
-          ref={navCheck}
-        />
-      </div>
+      <i
+        className="fas fa-bars  text-[red] text-lg md:text-xl lg:text-2xl block md:hidden"
+        onClick={() => onClickHandler()}
+      ></i>
     </div>
   );
 };
