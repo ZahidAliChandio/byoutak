1972f702beb49b444b150e5158904bdb2c6d8882
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-24 01:05:47 +0500

    Some progress in search tab.

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,14 +1,16 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import PropertyCard from "./PropertyCard";
 import SearchBar from "./SearchBar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Grid } from "swiper";
-import Apartment from "../../static/images/apartment.jpg"
+import Apartment from "../../static/images/apartment.jpg";
 import "swiper/css";
 import "swiper/css/grid";
 import "swiper/css/pagination";
 
 const Search = () => {
+  const [slidesPerView,setSlidesPerView]=useState(0);
+
   const propertyData = [
     {
       id: 1,
@@ -118,37 +120,122 @@ const Search = () => {
       bathrooms: 2,
       area: "150 m²",
     },
-    
+    {
+      id: 10,
+      image: null,
+      title: "Twinhouse",
+      subtitle: "SODIC",
+      price: "EGP 5,500,000",
+      contient: "Sodic Ease",
+      location: "New Heliopolis, Egypt",
+      bedrooms: 3,
+      bathrooms: 2,
+      area: "150 m²",
+    },
+    {
+      id: 11,
+      image: null,
+      title: "Twinhouse",
+      subtitle: "SODIC",
+      price: "EGP 5,500,000",
+      contient: "Sodic Ease",
+      location: "New Heliopolis, Egypt",
+      bedrooms: 3,
+      bathrooms: 2,
+      area: "150 m²",
+    },
+    {
+      id: 12,
+      image: null,
+      title: "Twinhouse",
+      subtitle: "SODIC",
+      price: "EGP 5,500,000",
+      contient: "Sodic Ease",
+      location: "New Heliopolis, Egypt",
+      bedrooms: 3,
+      bathrooms: 2,
+      area: "150 m²",
+    },
+    {
+      id: 13,
+      image: null,
+      title: "Twinhouse",
+      subtitle: "SODIC",
+      price: "EGP 5,500,000",
+      contient: "Sodic Ease",
+      location: "New Heliopolis, Egypt",
+      bedrooms: 3,
+      bathrooms: 2,
+      area: "150 m²",
+    },
+    {
+      id: 14,
+      image: null,
+      title: "Twinhouse",
+      subtitle: "SODIC",
+      price: "EGP 5,500,000",
+      contient: "Sodic Ease",
+      location: "New Heliopolis, Egypt",
+      bedrooms: 3,
+      bathrooms: 2,
+      area: "150 m²",
+    },
   ];
+  useEffect(()=>{
+    if(window.innerWidth>=1200){
+      setSlidesPerView(4);
+    }
+    else if(window.innerWidth>=993){
+      setSlidesPerView(3);
+    }    
+    else if(window.innerWidth>=768){
+      setSlidesPerView(2);
+    }else{
+      setSlidesPerView(1);
+    }
+  },[])
   return (
-    <Fragment>      
+    <Fragment>
       <SearchBar />
       <div className="relative px-4 py-4">
         <div className="text-white text-center w-full">
           <h2 className="font-bold text-xl md:text-2xl lg:text-3xl lg:mb-6 px-4 sm:p-0 grid-cols-3">
             {propertyData.length} results
           </h2>
-        </div>      
-        <Swiper
-          slidesPerView={3}
-          grid={{ rows:2 }}
-          spaceBetween={14}
+        </div>
+
+        <Swiper          
+          grid={{ rows: (12/slidesPerView), fill: "row" }}          
+          breakpoints={{
+            1536: {
+              slidesPerView: propertyData.length > 4 ? 4 : propertyData.length,
+            },
+            1280: {
+              slidesPerView: propertyData.length >= 3 ? 3 : propertyData.length,
+            },
+            1024: {
+              slidesPerView: propertyData.length >= 2 ? 2 : propertyData.length,
+            },
+            768: {
+              slidesPerView: propertyData.length >= 2 ? 2 : propertyData.length,
+            },
+          }}
+          spaceBetween={20}          
           pagination={{
-            clickable: true,
+            clickable: true,            
           }}
           modules={[Grid, Pagination]}
           className="mySwiper !z-0"
         >
           {propertyData.map((data) => {
             return (
-              <SwiperSlide key={data.id} className="">
+              <SwiperSlide key={data.id}>
                 <PropertyCard data={data} />
               </SwiperSlide>
             );
           })}
         </Swiper>
-      {/* <PropertyCard data={propertyData[0]} />
-      <PropertyCard data={propertyData[1]} /> */}
+        <div className="my-3">aa</div>        
       </div>
     </Fragment>
   );
