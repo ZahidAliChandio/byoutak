d1a6a654c109c90460112f79f22c573adea179b2
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-25 23:15:37 +0500

    property card padding updated.

--- src/pages/Property/PropertyCard.js
+++ src/pages/Property/PropertyCard.js
@@ -4,11 +4,11 @@ const PropertyCard = (props) => {
   return (
     <a href="/" className="relative overflow-hidden w-fit">
       <img src={data.image} alt="location" className="min-h-[285px] object-cover" />
-      <div className="absolute bottom-0 w-fit p-3">
+      <div className="absolute bottom-0 w-full px-3 md:px-4 lg:px-6 py-4">
         <img src={data.logo} alt="hyde" className="h-16 w-16"/>
         <h3 className="font-semibold mt-2">{data.title}</h3>
         <h4>{data.location}</h4>
-        <div className="text-end">
+        <div className="flex flex-col items-end mb-2">
           <p>Starting Price</p>
           <span className="font-bold">{data.price}</span>
         </div>
