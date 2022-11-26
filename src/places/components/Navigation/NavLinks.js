1785a6deaf2f2bb06979bf22885cfe39e482d506
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-27 03:12:04 +0500

    Admin drawer near to complete.

--- src/places/components/Navigation/NavLinks.js
+++ src/places/components/Navigation/NavLinks.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavLinks = (props) => {
@@ -7,46 +7,92 @@ const NavLinks = (props) => {
     {
       title: "Dashboard",
       iconClass: "fas fa-tachometer-alt-fast",
+      isLi: false,
       linkTo: "",
     },
     {
       title: "Projects",
       iconClass: "fas fa-flag",
+      isLi: true,
       linkTo: "",
     },
     {
       title: "Properties",
       iconClass: "fas fa-building",
+      isLi: true,
       linkTo: "",
     },
   ];
+  const subList = [
+    {
+      content: null,
+    },
+    {
+      content: [{ title: "Add Projects" }, { title: "View all Projects" }],
+    },
+    {
+      content: [
+        { title: "Add New Property" },
+        { title: "View all Properties" },
+      ],
+    },
+  ];
 
-  const onClickHander = (index) => {
-    setActiveLinkIndex(index);
+  const activeSubList = activeLinkIndex
+    ? subList[activeLinkIndex].content
+    : null;
+
+  const listItemClickHandler = (index) => {
+    setActiveLinkIndex((prev) => (prev === index ? null : index));
   };
 
   return (
-    <ul className="nav-links flex flex-col items-center w-full h-full bg-[#212020] mt-4">
-      {lis.map((li, index) => (
-        <li className="w-full">
-          <NavLink
-            to=""
-            exact
-            className={`${
-              activeLinkIndex === index
-                ? "font-semibold bg-gray-400 text-[#212020]"
-                : "text-gray-100"
-            } flex items-center hover:font-semibold gap-4 w-full text-xs px-4 py-2`}
-            onClick={() => onClickHander(index)}
-          >
-            <i className={`${li.iconClass} pr-2`}></i>
-            <span className="">{li.title}</span>
-          </NavLink>
-        </li>
-      ))}
-
-      {/* <button className="border text-white bg-transparent hover:bg-[#f8df00] hover:text-[#292929] active:text-[#292929] active:bg-[#f8df00]"></button> */}
-    </ul>
+    <Fragment>
+      <ul className="nav-links flex flex-col items-center w-full h-full bg-[#212020] mt-4">
+        {lis.map((li, index) => {
+          return (
+            <Fragment key={index}>
+              <li
+                className={`relative ${
+                  activeLinkIndex === index
+                } flex items-center justify-between text-xs  bg-gray-300 py-3 cursor-pointer font-gillsans px-4 border border-[#212020] text-[#212020] w-full`}
+                onClick={() => listItemClickHandler(index)}
+                key={index}
+              >
+                {" "}
+                <div className="flex items-center gap-4">
+                  <i className={`${li.iconClass} `}></i>
+                  <span>{li.title}</span>
+                </div>
+                <div className="flex gap-12 items-center">
+                  {li.isLi && (
+                    <i
+                      className={`fas fa-chevron-left ${
+                        activeLinkIndex === index ? "-rotate-90" : "rotate-0"
+                      } transition-all duration-500`}
+                    ></i>
+                  )}
+                </div>
+              </li>
+              {activeSubList && activeLinkIndex === index && (
+                <ul className="flex flex-col gap-1 text-gray-300 my-2 font-semibold">
+                  {activeSubList.map((content, index) => {
+                    return (
+                      <li key={index} className="">
+                        <NavLink to={""} className="flex gap-2 text-gray-300 text-xs">
+                          <i className="fal fa-chevron-double-right text-[red]"></i>
+                          <span className="">{content.title}</span>
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </Fragment>
+          );
+        })}
+      </ul>
+    </Fragment>
   );
 };
 
