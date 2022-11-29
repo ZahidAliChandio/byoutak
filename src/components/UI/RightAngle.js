546c80bf0d4c8e9437f4907254b6a161780283e9
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-30 01:48:14 +0500

    minor error fix.

--- src/components/UI/RightAngle.js
+++ src/components/UI/RightAngle.js
@@ -1,6 +1,6 @@
 const RightAngle=()=>{
     return(
-        <div className="absolute left-0% top:10% sm:left-5% sm:top-8% md:left-15% md:top-10% flex flex-col items-center m-auto mt-7 lg:mt-9 pt-8 sm:pt-10 lg:pt-16 sm:w-11/12 md:w-10/12">
+        <div className="absolute left-0 top:[10%] sm:left-[5%] sm:top-[8%] md:left-[15%] md:top-[10%] flex flex-col items-center m-auto mt-7 lg:mt-9 pt-8 sm:pt-10 lg:pt-16 sm:w-11/12 md:w-10/12">
         <div className="absolute top-0 left-0">
           <div className="relative ml-3 sm:m-auto">
             <div className="absolute left-0 top-0 h-4 sm:h-5 w-16 sm:w-24 md:w-28 md:h-6 bg-[red] z-20"></div>
