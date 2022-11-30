ede74c092ebeb0614793f72dcc867a1d9dadd50a
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-30 18:36:30 +0500

    sliding content updated.

--- src/components/UI/RightAngle.js
+++ src/components/UI/RightAngle.js
@@ -1,6 +1,6 @@
 const RightAngle=()=>{
     return(
-        <div className="absolute left-[5%] top-[15%] sm:left-[10%] sm:top-[15%] md:left-[10%] md:top-[15%] flex flex-col items-center">
+        <div className="absolute left-[3%] top-[12%] sm:left-[10%] sm:top-[15%] md:left-[10%] md:top-[15%] flex flex-col items-center">
         <div className="absolute top-0 left-0">
           <div className="relative ml-3 sm:m-auto">
             <div className="absolute left-0 top-0 h-4 sm:h-5 w-16 sm:w-24 md:w-28 md:h-6 bg-[red] z-20"></div>
